45b04a60dc16ffc118db94ef264e7998b7160088
miliasv <milias.vasilis@gmail.com>
2016-12-02 19:18:44 +0100

    no changes

--- scatterplot.js
+++ scatterplot.js
@@ -58,19 +58,19 @@ function getDataPerYear(csv_data) {
 
 function monthsToNumbers(month){
    switch (month) {
-                case "December": month = 12; break;
-                case "January": month = 01; break;
-                case "February": month = 02; break;
-                case "March": month = 03; break;
-                case "April": month = 04; break;
-                case "May": month = 05; break;
-                case "June": month = 06; break;
-                case "July": month = 07; break;
-                case "August": month = 08; break;
-                case "September": month = 09; break;
-                case "October": month = 10; break;
-                case "November": month = 11; break;
-              } 
+      case "December": month = 12; break;
+      case "January": month = 01; break;
+      case "February": month = 02; break;
+      case "March": month = 03; break;
+      case "April": month = 04; break;
+      case "May": month = 05; break;
+      case "June": month = 06; break;
+      case "July": month = 07; break;
+      case "August": month = 08; break;
+      case "September": month = 09; break;
+      case "October": month = 10; break;
+      case "November": month = 11; break;
+      } 
   return month
 
 }
