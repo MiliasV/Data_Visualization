7fd738bf60359d579d347a104986800e14a0ef9f
miliasv <milias.vasilis@gmail.com>
2016-12-10 14:07:40 +0100

    Incoming Refugees added

--- barplot_scatterplot.js
+++ barplot_scatterplot.js
@@ -5,10 +5,11 @@ var svg = d3.select("svg"),
     height = +svg.attr("height") - margin.top - margin.bottom;
     console.log(height)
 
+var svg1 = d3.select("#barplotOutgoing") 
+var svg2 = d3.select("#barplotIncoming") 
+
 var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
     y = d3.scaleLinear().rangeRound([height, 0]);
-
-
 
 var refugeesPath = "data/asylum_seekers_monthly_all_data2.csv";
 
@@ -19,10 +20,11 @@ data = csv_data
 createDropdown(data);
 });
 
+
 //Creation of the Dropdown menu, with the data from
 //the column that is specified in the variable menu
 function createDropdown(csv_data){
-  var menu = ["OriginCountries", "UniqueYear"]
+  var menu = ["OriginCountries", "UniqueYear", "Ingoing", "UniqueYear"]
   menu.forEach(function(value, index, array){
     csv = csv_data.filter(function(row){
          // console.log(row[value])
@@ -44,40 +46,68 @@ function createDropdown(csv_data){
 //Initialization - global variables
 var country ="Afghanistan"
 var year = 1999
+var flag = 0 //flag to define if we draw a barplot for 
+                // outgoing or incoming refugees
+                //flag=0 => outgoing
+                //flag=1 => incoming
 
 //If someone changes the country, the drawBarplot is called
 //with input this country and the year that has been selected last
 function dropdownSelectionOutgoing(s){
   country = s[s.selectedIndex].id; 
-  drawBarplot(country,year)
+  drawBarplot(country,year, 0) //the third argument is the flag
 }
 
 //If someone changes the year, the drawBarplot is called
 //with input this year and the country that has been selected last
-function dropdownSelectionYear(s){
+function dropdownSelectionYearOutgoing(s){
   year = s[s.selectedIndex].id; 
- drawBarplot(country, year)
+ drawBarplot(country, year, 0) //the third argument is the flag
 }
 
-//return the data for a specific country in a specific year
-function getcountriesPerOriginPerYear(data, Origin, Year){
+function dropdownSelectionIncoming(s){
+  country = s[s.selectedIndex].id; 
+  drawBarplot(country,year, 1) //the third argument is the flag
+}
 
+//If someone changes the year, the drawBarplot is called
+//with input this year and the country that has been selected last
+function dropdownSelectionYearIncoming(s){
+  year = s[s.selectedIndex].id; 
+ drawBarplot(country, year, 1) //the third argument is the flag
+}
+
+
+//return the data for a specific country in a specific year
+function getcountriesPerOriginPerYear(data, Origin, Year, flag){
+
+  if (flag==0){
+    var fromColumn = "Origin"
+    var toColumn = "Country"
+  }
+  else{
+    var fromColumn = "Country"
+    var toColumn = "Origin"
+  }
   var countriesPerOrigin = d3.nest()
-            .key(function(d) {return d.Origin})
+            .key(function(d) {return d[fromColumn]})
             .key(function(d){ return d.Year})
-            .key(function(d) {return d.Country})
+            .key(function(d) {return d[toColumn]})
             .rollup(function(d) {return d3.sum(d, function(g){return g.Value})})
             .entries(data)
             .filter(function(d){ return d.key == Origin})
   //console.log(countriesPerOrigin)
-
   return countriesPerOrigin[0].values.filter(function(d){return d.key == Year})[0].values
-
-
 }
 
-function drawBarplot(Origin, Year){
+function drawBarplot(Origin, Year, flag){
 
+if (flag==0){
+  svg = svg1
+}
+else{
+  svg=svg2
+}
 //remove the previous barblot
 svg.selectAll("g").remove()
 
@@ -86,7 +116,7 @@ var g = svg.append("g")
 
  console.log(Origin, Year)
  //Get the data
- var countriesPerOriginPerYear = getcountriesPerOriginPerYear(data, Origin, Year)
+ var countriesPerOriginPerYear = getcountriesPerOriginPerYear(data, Origin, Year, flag)
 
 //  console.log(countriesPerOriginPerYear)
 
